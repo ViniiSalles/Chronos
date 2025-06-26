18464210567eab87925693861bb88bcf26c3b905
Vinicius Salles de Oliveira <56527570+ViniiSalles@users.noreply.github.com>
2025-06-26 13:14:58 -0300

    ..

--- code/backend/gateway.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MyGateway } from './gateway';
-import { UserModule } from 'src/user/user.module';
-
-@Module({
-  providers: [MyGateway, UserModule],
-  imports: [UserModule],
-})
-export class GatewayModule {}
